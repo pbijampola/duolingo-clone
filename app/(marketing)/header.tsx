06342a7298075e4922a5cb64281c24f77bafea56
pbijampola <patrickbijampola@gmail.com>
2024-05-01 12:05:52 +0300

    Structured the marketing or landing page layout by providing header and footer components

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,10 @@
+export const Header =()=>{
+    return (
+        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+            <div className="flex h-full items-center justify-between">
+                <h1 className="text-3xl font-bold">Duolingo Clone</h1>
+                <p>site language</p>
+            </div>
+        </header>
+    )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div >
+      <p className="">Hello Duolingo Clone, this is the marketing page</p>
+      
+    </div>
+  )
+}
