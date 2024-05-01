44490b085ccaa484a21a403c0fb290f42c3b8ec5
pbijampola <patrickbijampola@gmail.com>
2024-05-01 13:59:29 +0300

    Added authentication using clerk and designed the hero section on the landing page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,30 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import { Loader } from "lucide-react"
+import { SignedIn,SignedOut,SignInButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+
 export const Header =()=>{
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className="flex h-full items-center justify-between">
-                <h1 className="text-3xl font-bold">Duolingo Clone</h1>
-                <p>site language</p>
+            <div className="lg:max-w-screen-lg mx-auto flex h-full items-center justify-between ">
+               <div className="pt-8 pl-4 flex item-center gap-x-3">
+                <Image src="/lingo-logo.svg" alt="duolingo logo" width={150} height={150} className="rounded"/>
+                {/* <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">duolingo Clone</h1> */}
+               </div>
+               <ClerkLoading>
+                <Loader className=" h-5 w-5 text-muted-foreground animate-spin"/>
+               </ClerkLoading>
+               <ClerkLoaded>
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/"/>
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                        <Button size="lg" variant="ghost">Login</Button>
+                    </SignInButton>
+                </SignedOut>
+               </ClerkLoaded>
             </div>
         </header>
     )
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,48 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn ,SignedOut} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Link from "next/link";
+
 
 
 export default function Home() {
   return (
-    <div >
-      <p className="">Hello Duolingo Clone, this is the marketing page</p>
-      
+    <div className="max-w-[988px] mx-auto flex-1 flex flex-col w-full lg:flex-row items-center justify-center p-4 gap-2 ">
+     <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+      <Image src="/header.svg" alt="header" fill/>
+     </div>
+     <div className="flex flex-col items-center justify-center">
+      <h1 className="text-lg lg:text-3xl font-bold text-neutral-600 max-w-[428px] text-center">The free, fun, and effective way to learn a language!</h1>
+     
+      <div>
+        <ClerkLoading>
+          <Loader className=" h-5 w-5 text-muted-foreground animate-spin"/>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignUpButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+              <Button variant="secondary" size="lg" className="w-full mt-6">Get Started</Button>
+            </SignUpButton>
+
+            <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+              <Button variant="primaryOutline" size="lg" className="w-full mt-6">I already have an account</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            
+            <Button size="lg" variant="secondary" className="w-full mt-6" asChild>
+              <Link href="/learn">
+                Continue Learning
+                </Link>
+  
+              </Button>
+          </SignedIn>
+        </ClerkLoaded>
+      </div>
+     
+     </div>
+     
     </div>
   )
 }
